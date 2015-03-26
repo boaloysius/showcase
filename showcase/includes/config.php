5d056a24d5f840a97b13c9a783520e7fe654a615
boaloysius <boaloysius@gmail.com>
2015-03-26 15:53:45 +0530

    some files added

---
+++ showcase/includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database Constants
+defined('DB_SERVER') ? null : define("DB_SERVER", "your_server");
+defined('DB_USER')   ? null : define("DB_USER", "your_username");
+defined('DB_PASS')   ? null : define("DB_PASS", "your_password");
+defined('DB_NAME')   ? null : define("DB_NAME", "your_database_name");
+
+?>
---
+++ showcase/public/layouts/admin_header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>SHOW CASE: Admin</title>
+    <link href="../stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="header">
+      <h1>SHOW CASE: Admin</h1>
+    </div>
+    <div id="main">
